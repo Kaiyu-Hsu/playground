4d2f4121d518fb1a1f43bf65e22104a105cb0203
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-11-07 18:31:34 +0800

    update: el-btn + v-tippy

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="main">
-    <el-popover
-      placement="top-start"
-      title="标题"
-      width="200"
-      trigger="hover"
-      content="內容"
-    >
-      <el-button id="myButton" slot="reference">hover 激活</el-button>
-    </el-popover>
+    <el-button type="text" v-tippy="tippyConfig()">
+      <i class="el-icon-more"></i>
+    </el-button>
   </div>
 </template>
 
 <script>
+import "tippy.js/themes/light.css";
+
 export default {
   name: "Main",
   props: {},
-  data() {},
+  data() {
+    return {
+      content: `<el-button type="primary"><i class="el-icon-crop"></i>數據集</el-button>`,
+    };
+  },
+  methods: {
+    tippyConfig() {
+      let config = {
+        content: this.content,
+        allowHTML: true,
+        theme: "light",
+        arrow: false,
+        placement: "bottom",
+      };
+
+      return config;
+    },
+  },
 };
 </script>
 
