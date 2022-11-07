f17580d0e76240b73b9c53351b00e27baef4d353
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-11-07 18:27:08 +0800

    add: vue-tippy.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import VueTippy from "vue-tippy";
+
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.use(VueTippy, {
+  arrow: true,
+  ignoreAttributes: true,
+  allowHTML: false
+})
 
 new Vue({
   router,
