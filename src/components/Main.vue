608a9c1ac1ceadd32dfe7c9aeeb8bb741b136d0d
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-11-07 17:23:41 +0800

    add: main page

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <el-popover
+      placement="top-start"
+      title="标题"
+      width="200"
+      trigger="hover"
+      content="內容"
+    >
+      <el-button id="myButton" slot="reference">hover 激活</el-button>
+    </el-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {},
+  data() {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
