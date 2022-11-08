2fd9bc6f3d181614fa1a3236c19edc5e87d08675
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-11-08 11:50:48 +0800

    add: navigation

---
+++ src/components/modules/organisms/TheLayout/TheNavigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="navigation">
+    <!-- :collapse="isCollapse" -->
+    <el-menu
+      class="el-menu-vertical-demo"
+      :default-active="activeIndex"
+      @open="handleOpen"
+      @close="handleClose"
+      @select="handleSelect"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span slot="title">小工具</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1"
+            >el-tooltip + el-cascader-panel</el-menu-item
+          >
+          <el-menu-item index="1-2">element + vue-tippy</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // isCollapse: Boolean,
+  },
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// .el-menu-vertical-demo:not(.el-menu--collapse) {
+//   width: 200px;
+//   min-height: 400px;
+// }
+</style>
--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,3 +1,36 @@
 <template>
-  <div class="example">123</div>
+  <div class="example">
+    <el-container>
+      <!-- :width="isCollapse ? '64px' : '200px'" -->
+      <el-aside>
+        <!-- :isCollapse="isCollapse" -->
+        <TheNavigation />
+      </el-aside>
+    </el-container>
+  </div>
 </template>
+
+<script>
+import TheNavigation from "../components/modules/organisms/TheLayout/TheNavigation.vue";
+export default {
+  components: {
+    TheNavigation,
+  },
+  data() {
+    return {
+      isCollapse: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.el-container,
+.el-aside {
+  height: 100%;
+}
+
+.el-aside {
+  overflow: hidden;
+}
+</style>
