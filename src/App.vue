a134db9040e9d202d714aa2607381e37f1485633
DESKTOP-BHJQSNO\cindy <cindy@profetai.com>
2022-11-08 11:04:21 +0800

    add: example page & route

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/example">Example</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/views/Example.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="example">123</div>
+</template>
